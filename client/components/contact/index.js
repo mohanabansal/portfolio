14b5cc82f8ac5bc89f5f30684f4572d2ff1dbc2e
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-01 15:37:05 -0400

    working on contact page

--- client/components/contact/index.js
+++ client/components/contact/index.js
@@ -5,10 +5,17 @@ import './index.css'
 const Contact = () => {
   return (
     <div id="contact">
-      <h1 className="heading">Contact</h1>
-      <a href="" target="_blank">
-        <img src="./linkedIn.png" />
-      </a>
+      <h1 className="heading">Let's connect</h1>
+      <h2 className="contact-subheading">I'd love to hear from you</h2>
+      <p className="contact-description">
+        Whether you want to get in a technical discussion, talk about any
+        project, discussion a potential job opportunity or anything else, I am
+        here. <br />
+        <br />
+        You can find on either of the following platform:{' '}
+      </p>
+      <a href="https://www.linkedin.com/in/mohanabansal/" target="_blank" />
+      <a href="https://github.com/mohanabansal/" target="_blank" />
     </div>
   )
 }
