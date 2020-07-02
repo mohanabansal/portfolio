dd2d169f5c1c2526235baba118ae96a711ae1df9
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-02 14:30:11 -0400

    added resume to website

--- client/components/resume/index.js
+++ client/components/resume/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import resume from '../constants/MohanaBansalResume.pdf'
 import './index.css'
 
 const Resume = () => {
   return (
     <div id="resume">
       <h1 className="heading"> Resume</h1>
-      <a href={resume} target="_blank">
+      <a href="/MohanaBansalResume.pdf" target="_blank" type="button">
         View Resume
       </a>
-      <a href={resume} download target="_blank" type="button">
+      <a href="/MohanaBansalResume.pdf" download target="_blank" type="button">
         Download Resume
       </a>
     </div>
