8740079c08d9f6e79eda81dbc4298d9c699fa543
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-02 16:24:23 -0400

    fixed menubar on scroll

--- client/components/menubar/index.js
+++ client/components/menubar/index.js
@@ -4,15 +4,15 @@ import './index.css'
 const Menubar = () => {
   return (
     <div id="menu">
-      <span>
-        <a href="/#">Home</a>
-      </span>
-      <span>
-        <a href="/#contact">Contact</a>
-      </span>
-      <span>
-        <a href="/#resume">Resume</a>
-      </span>
+      {/* <span> */}
+      <a href="#">Home</a>
+      {/* </span> */}
+      {/* <span> */}
+      <a href="#contact">Contact</a>
+      {/* </span> */}
+      {/* <span> */}
+      <a href="#resume">Resume</a>
+      {/* </span> */}
     </div>
   )
 }
