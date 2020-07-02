b50668297898f2418b382d37f36dbb40b431a252
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-02 15:49:15 -0400

    added footer and menubar skeleton

---
+++ client/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <span>© Mohana Bansal, 2020 All rights reserved.</span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/components/menubar/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+
+const Menubar = () => {
+  return (
+    <div id="menu">
+      <span>
+        <a href="/#">Home</a>
+      </span>
+      <span>
+        <a href="/#contact">Contact</a>
+      </span>
+      <span>
+        <a href="/#resume">Resume</a>
+      </span>
+    </div>
+  )
+}
+
+export default Menubar
