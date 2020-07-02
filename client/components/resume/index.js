67423191fbe2a61545710f8a26c2a578e38480b8
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-02 15:15:04 -0400

    added styling to buttons

--- client/components/resume/index.js
+++ client/components/resume/index.js
@@ -5,12 +5,18 @@ const Resume = () => {
   return (
     <div id="resume">
       <h1 className="heading"> Resume</h1>
-      <a href="/MohanaBansalResume.pdf" target="_blank" type="button">
-        View Resume
-      </a>
-      <a href="/MohanaBansalResume.pdf" download target="_blank" type="button">
-        Download Resume
-      </a>
+      <div className="resume-button">
+        <button type="button">
+          <a href="/MohanaBansalResume.pdf" target="_blank">
+            View Resume
+          </a>
+        </button>
+        <button type="button">
+          <a href="/MohanaBansalResume.pdf" download target="_blank">
+            Download Resume
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
