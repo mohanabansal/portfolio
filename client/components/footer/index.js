9c000bd6e81bdffa1fdb192a974da3af6ac08f89
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-02 16:06:09 -0400

    updated menubar and footer css

--- client/components/footer/index.js
+++ client/components/footer/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './index.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer">
       <span>© Mohana Bansal, 2020 All rights reserved.</span>
     </div>
   )
