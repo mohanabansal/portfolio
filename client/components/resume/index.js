2d959da98e8f4b7ca788ad5f9eb9cd690a9f0186
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-02 13:55:46 -0400

    added pdf loaders and pdf resume

--- client/components/resume/index.js
+++ client/components/resume/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import resume from '../../resumeContainer/MohanaBansalResume.pdf'
+import resume from '../constants/MohanaBansalResume.pdf'
 import './index.css'
 
 const Resume = () => {
   return (
     <div id="resume">
       <h1 className="heading"> Resume</h1>
-      <a href={resume} target="_blank" type="button">
+      <a href={resume} target="_blank">
         View Resume
+      </a>
+      <a href={resume} download target="_blank" type="button">
+        Download Resume
       </a>
     </div>
   )
