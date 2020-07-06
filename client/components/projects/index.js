b49b3e771ae520002211bb49d5f73bf9fd49d6cc
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-05 21:26:29 -0400

    WIP - styling of project

--- client/components/projects/index.js
+++ client/components/projects/index.js
@@ -5,9 +5,22 @@ const Projects = () => {
   return (
     <div id="projects">
       <h1 className="heading">Projects</h1>
+      <h1 className="title">Diagon-E-lley</h1>
       <div className="project-description">
         <div className="description">
-          <h1 className="title">Diagon-E-lley</h1>
+          {/* <div className = "heading-and-links"> */}
+
+          {/* <div className="links">
+              <button type="button" className="btn">
+                <a>Visit Website</a>
+                <span></span>
+              </button>
+              <button type="button" className="btn">
+                <a>GitHub</a>
+                <span></span>
+              </button>
+            </div> */}
+          {/* </div> */}
           <p>
             A one stop shop for your next year essentials at Hogwarts School of
             Witchcraft and Wizardry
@@ -15,33 +28,41 @@ const Projects = () => {
           <p>
             Diagon e-lley is a fullstack mock e-commerce site, built around the
             theme of a Hogwarts campus supply store. This project was built over
-            ten days.All users can browse all products, filter products by
+            ten days. All users can browse all products, filter products by
             specific categories, add products to the cart, edit contents of the
             cart, "checkout" and "buy" products, have the cart automatically
-            save across page refreshes
+            sync across multiple clients, receive a purchase confirmation email,
+            view previous purchase history
           </p>
-          <p>
+          {/* <p>
             Logged-in users can update their profile, have cart automatically
             sync among multiple clients, have cart be saved after logout,
             receive a purchase confirmation email, view previous purchase
             history
-          </p>
+          </p> */}
           <p>
             Administrators have these additional capabilities add, update, and
             delete inventory, view all signed up users
           </p>
         </div>
         <div>
-          <img src="/diagonelley.png" />
+          <img src="/diagonmockupnew.png" />
         </div>
       </div>
-      <div className="meta-data">
-        <div className="links">
-          <button type="button" className="btn">
-            Visit Website<span></span>
-          </button>
-        </div>
+      <div className="tech">
+        <p>
+          Technologies used: <span>ReactJS</span>
+          <span>Redux</span>
+          <span>JavaScript</span>
+          <span>Node.js</span>
+          <span>Express</span>
+          <span>Sequelize</span>
+          <span>PostgresSQL</span>
+          <span>HTML</span>
+          <span>CSS</span>
+        </p>
       </div>
+      <div className="meta-data"></div>
     </div>
   )
 }
