ac61dba8059fd046b5449fc4b60fef0fa711a3d4
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-06 20:36:24 -0400

    updating UI

--- client/components/projects/index.js
+++ client/components/projects/index.js
@@ -32,24 +32,46 @@ const Projects = () => {
             specific categories, add products to the cart, edit contents of the
             cart, "checkout" and "buy" products, have the cart automatically
             sync across multiple clients, receive a purchase confirmation email,
-            view previous purchase history
+            view previous purchase history. Administrators have additional
+            capabilities to add, update, and delete inventory, view all signed
+            up users.
           </p>
+          <span className="tech">
+            <p>
+              <span>ReactJS</span>
+              <span>Redux</span>
+              <span>JavaScript</span>
+              <span>Node.js</span>
+              <span>Express</span>
+              <span>Sequelize</span>
+              <span>PostgresSQL</span>
+              <span>HTML</span>
+              <span>CSS</span>
+            </p>
+          </span>
           {/* <p>
             Logged-in users can update their profile, have cart automatically
             sync among multiple clients, have cart be saved after logout,
             receive a purchase confirmation email, view previous purchase
             history
           </p> */}
-          <p>
-            Administrators have these additional capabilities add, update, and
-            delete inventory, view all signed up users
-          </p>
+          <p></p>
         </div>
         <div>
           <img src="/diagonmockupnew.png" />
+          <div className="links">
+            <button type="button" className="btn">
+              <a>Visit Website</a>
+              <span></span>
+            </button>
+            <button type="button" className="btn">
+              <a>GitHub</a>
+              <span></span>
+            </button>
+          </div>
         </div>
       </div>
-      <div className="tech">
+      {/* <div className="tech">
         <p>
           Technologies used: <span>ReactJS</span>
           <span>Redux</span>
@@ -61,7 +83,7 @@ const Projects = () => {
           <span>HTML</span>
           <span>CSS</span>
         </p>
-      </div>
+      </div> */}
       <div className="meta-data"></div>
     </div>
   )
