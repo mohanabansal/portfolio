fb728be5c28844febb37ed8b2ee39f53e32c8502
Mohana Bansal <mohanabansal10@gmail.com>
2020-07-14 23:14:09 -0400

    WIP - responsive menubar

--- client/components/menubar/index.js
+++ client/components/menubar/index.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {FaBars} from 'react-icons/fa'
 import './index.css'
 
-const Menubar = () => {
-  return (
-    <div id="menu">
-      <a href="#">Home</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
-      <a href="#resume">Resume</a>
-    </div>
-  )
+class Menubar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      clicked: false,
+    }
+  }
+
+  handleClick = () => {
+    console.log('clicked')
+    this.setState({
+      clicked: !this.state.clicked,
+    })
+  }
+  render() {
+    return (
+      <div id={this.state.clicked ? 'menu-open' : 'menu'}>
+        <button type="button" onClick={this.handleClick}>
+          <FaBars />
+        </button>
+        {/* <div className = "menu-items"> */}
+        <a href="#">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+        <a href="#resume">Resume</a>
+        {/* <a>
+          <button type="button">Click</button>
+        </a> */}
+        {/* </div> */}
+      </div>
+    )
+  }
 }
 
 export default Menubar
